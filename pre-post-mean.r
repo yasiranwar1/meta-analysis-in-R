7c6da9d500222797de01409bce56025bccf050a2
yasiranwar1 <66573998+yasiranwar1@users.noreply.github.com>
2020-07-25 02:07:14 +0500

    Update pre-post-mean.r

--- pre-post-mean.r
+++ pre-post-mean.r
@@ -1,4 +1,10 @@
-data <- read.csv(file.choose())
+data <- data <- structure(list(Study = c("Khaled, 2019", "El-Haggar,2018", "Eghbali,2017", 
+                                 "Fernandes, 2013"), n.e = c(20L, 20L, 29L, 5L), Before_mean.e = c(40.63, 
+                                  21.9, 21.9, 21.7), Before_Std.e = c(5.45, 8.7, 8, 7.2), After_mean.e = c(43.26, 
+                                  24.6, 24.5, 28.2), After_std.e = c(5.35, 9.4, 7.6, 7.9), n.c = c(20L, 
+                                  20L, 27L, 10L), Before_mean.c = c(53.23, 21.8, 23.9, 24.7), Before_Std.c = c(6.61, 
+                                  7.7, 7.6, 7.8), After_mean.c = c(52.99, 22.94, 21.1, 26.2), After_std.c = c(6.6, 
+                                  7.1, 7.3, 11.4)), row.names = c(NA, 4L), class = "data.frame")
 
 data$mean.change.e <- data$After_mean.e - data$Before_mean.e
 
